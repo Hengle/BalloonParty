39a42da4e415b1e5a35b63dc28bf306d5f524868
José Villegas <villegasjose.gg@gmail.com>
2020-09-29 11:29:00 -0300

    Colliding and destroying balloons

---
+++ Assets/Source/Balloon/BalloonCollisionSystem.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Entitas;
+using UnityEngine;
+
+public class BalloonCollisionSystem : ReactiveSystem<GameEntity>
+{
+    private readonly Contexts _contexts;
+    private readonly int _layer;
+
+    public BalloonCollisionSystem(Contexts contexts) : base(contexts.game)
+    {
+        _contexts = contexts;
+        _layer = LayerMask.NameToLayer("Balloons");
+    }
+
+    protected override ICollector<GameEntity> GetTrigger(IContext<GameEntity> context)
+    {
+        return context.CreateCollector(GameMatcher.AllOf(GameMatcher.FreeProjectile, GameMatcher.TriggerEnter2D));
+    }
+
+    protected override bool Filter(GameEntity entity)
+    {
+        return entity.isFreeProjectile && entity.hasTriggerEnter2D;
+    }
+
+    protected override void Execute(List<GameEntity> entities)
+    {
+        foreach (var gameEntity in entities)
+        {
+            var collider = gameEntity.triggerEnter2D.Value;
+
+            // we are colliding with balloons
+            if ((collider.gameObject.layer & _layer) > 0)
+            {
+                var linkedView = collider.GetComponent<ILinkedView>();
+                var balloonEntity = linkedView.LinkedEntity as GameEntity;
+
+                balloonEntity.isDestroyed = true;
+            }
+        }
+    }
+}
--- Assets/Source/Game/GameFixedUpdateSystems.cs
+++ Assets/Source/Game/GameFixedUpdateSystems.cs
@@ -2,6 +2,9 @@ public class GameFixedUpdateSystems : Feature
 {
     public GameFixedUpdateSystems(Contexts contexts)
     {
+        // collisions
+        Add(new BalloonCollisionSystem(contexts));
+
         // clean up
         Add(new Cleanup2DTriggersSystem(contexts));
     }
